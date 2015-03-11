6a50feb28d7b73c6b00672368821add54ef7812d
Aymeric <aymeric.agre@gmail.com>
2015-03-11 09:29:12 +0100

    petites modifs
    app.configure n'existe plus pour express 4.0
    ajout nodemon dans npm (redémarrage automatique serveur)
    ajout bower (installation package frontend)

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
  * Created by aymeric on 11/03/2015.
  */
 var express = require('express'),
+    bodyParser = require('body-parser');
     app = express();
 server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
-app.configure(function(){
-    app.use(express.static(__dirname + '/public'));
-});
+/**
+ * Definition of middlewares
+ */
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
 
 server.listen(3000);
