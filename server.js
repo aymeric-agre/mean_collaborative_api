070e09d2c17cd8d4a34a1a98b9ea843c346e2e47
Aymeric <aymeric.agre@gmail.com>
2015-03-11 12:12:58 +0100

    chat multiple

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     cons = require('consolidate'),
     swig = require('swig'),
     app = express();
-server = require('http').createServer(app),
+var server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
 /**
@@ -25,20 +25,39 @@ app.use(bodyParser());
  */
 app.route('/*')
     .get(function(req, res){
-    res.render('index');
+        res.render('index');
     })
     .post(function(req, res) {
-	res.redirect('/'+req.body.room);
-	console.log('You sent the code "' + req.body.room + '".');
+        res.redirect('/'+req.body.room);
+        console.log('You sent the code "' + req.body.room + '".');
     });
 
 /**
  * Sockets
  */
-io.on('connection', function (socket) {
-  console.log('client connected');
-  socket.emit('message', { content: 'Vous êtes bien connecté !', importance: '1' });
+io.sockets.on('connection', function (socket) {
+    console.log('a user is connected');
+    socket.on('createNote', function(data){
+        console.log('message: ' + data);
+        io.emit('createNote', data);
+        socket.broadcast.emit(onNoteCreateData);
+    });
+
+    socket.on('updateNote', function(data){
+        socket.broadcast.emit(onNoteUpdateData);
+    });
+
+    socket.on('deleteNote', function(data){
+        socket.broadcast.emit(onNoteDeleteData);
+    });
+
+    socket.on('moveNote', function(data){
+        socket.broadcast.emit(onNoteMoveData);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
- 
- 
+
 server.listen(3000);
