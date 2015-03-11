3eeade038635dd39567182d68e85823f452bc13f
baptiste.collette <baptiste.collette@ecl13.ec-lyon.fr>
2015-03-11 12:14:42 +0100

    Test salles
    Ne marche pas : les socket ne savent jamais où elles sont

--- server.js
+++ server.js
@@ -23,22 +23,47 @@ app.use(bodyParser());
 /**
  * Routing
  */
-app.route('/*')
-    .get(function(req, res){
-    res.render('index');
+var numeroSalle = ""; //On récupère le numero de la salle
+
+app.get('/*',function(req, res){
+		res.render('index');
+		console.log("GET");
     })
-    .post(function(req, res) {
-	res.redirect('/'+req.body.room);
-	console.log('You sent the code "' + req.body.room + '".');
+    .post('/*',function(req, res) {
+		res.redirect('/'+req.body.room);
+		console.log("POST");
+		numeroSalle=req.body.room;
     });
 
 /**
  * Sockets
  */
-io.on('connection', function (socket) {
-  console.log('client connected');
-  socket.emit('message', { content: 'Vous êtes bien connecté !', importance: '1' });
+io.sockets.on('connection', function (socket,room) {
+	console.log('client connected');
+	//Changement de la salle
+	socket.on('valeurSalle', function(room) {
+        socket.room = room;
+	});
+	
+	//On teste si on est dans une salle à chaque connexion
+	var str = numeroSalle.split("/"); //On sépare selon les "/"
+	str=str[str.length-1]; //On prend le dernier
+	var strLength = str.length; //On regarde sa taille
+	//Si on est dans un .html
+	if (strLength>10 || (strLength>4 && str.substring(strLength-5,strLength)==".html"))
+	{
+		console.log('room assigned');
+		socket.room = "";
+	}
+	else
+	{
+		console.log("str:"+str);
+		socket.room = str;
+	}
+	
+	socket.emit('message', { content: "Vous êtes bien connecté en salle "+socket.room, importance: '1', room: socket.room });
+	socket.broadcast.emit('message', { content: 'Un autre client vient de se connecter ! ', importance: '1', room: socket.room });
 });
- 
- 
+
+
 server.listen(3000);
