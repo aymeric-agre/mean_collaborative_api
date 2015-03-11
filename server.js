a66f631de6b7c87bed51262333afd7282f054006
Aymeric <aymeric.agre@gmail.com>
2015-03-11 10:01:48 +0100

    moteur de rendu consolidate/swig

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-/**
- * Created by aymeric on 11/03/2015.
- */
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    cons = require('consolidate'),
+    swig = require('swig'),
     app = express();
 server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-		
+
+/**
+ * Rendering engine
+ */
+app.engine('html', cons.swig);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public/views');
+
 /**
  * Definition of middlewares
  */
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
-app.post('/*', function(req, res) {
-	res.redirect('index.html/'+req.body.room);
-	res.send('You sent the code "' + req.body.room + '".');
-});
+/**
+ * Routing
+ */
+app.route('/*')
+    .get(function(req, res){
+    res.render('index');
+    })
+    .post(function(req, res) {
+	res.redirect('/'+req.body.room);
+	console.log('You sent the code "' + req.body.room + '".');
+    });
 
 server.listen(3000);
